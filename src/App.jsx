f2b50f9cf6c3efa47be4cde9d00fb203a3bad950
julio <juliopa54@gmail.com>
2024-04-16 17:48:40 -0400

    finished all logic, now its time to make it pretty

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,13 @@ const BASE_URL = 'https://api.fxratesapi.com/latest'
 
 function App() {
   const [currencyOptions, setCurrencyOptions] = useState([])
-  const [fromCurrency, setFromCurrency] = useState()
-  const [toCurrency, setToCurrency] = useState()
+  const [fromCurrency, setFromCurrency] =useState()
+  const [toCurrency, setToCurrency] =useState()
+
   const [exchangeRate, setExchangeRate] = useState()
   const [amount, setAmount] = useState(1)
-  const [amountInFromCurrency, setAmountInFromCurrency] = useState(true)
+  const [amountInFromCurrency, setAmountInFromCurrency] = useState(true )
+
 
   let toAmount, fromAmount
   if(amountInFromCurrency) {
@@ -25,7 +27,7 @@ function App() {
     fetch(BASE_URL)
       .then(res => res.json())
       .then(data => {
-        const firstCurrency = Object.keys(data.rates)[0]
+        let firstCurrency = Object.keys(data.rates)[52]
         setCurrencyOptions([data.base, ...Object.keys(data.rates)])
         setFromCurrency(data.base)
         setToCurrency(firstCurrency)
@@ -34,40 +36,40 @@ function App() {
   }, [])
 
   useEffect(() => {
-    if(fromCurrency != null && toCurrency != null) {
-        fetch(`${BASE_URL}?base=${fromCurrency}&symbols=${toCurrency}`)
-          .then(res => res.json())
-          .then(data => setExchangeRate(data.rates[toCurrency]))
-    }
+    if(fromCurrency && toCurrency != null) {
+      fetch(`${BASE_URL }?base=${fromCurrency}&symbols=${toCurrency}`)
+      .then(res => res.json())
+      .then(data => setExchangeRate(data.rates[toCurrency]))
+    } 
   }, [fromCurrency, toCurrency])
 
   function handleFromAmountChange(e) {
-    setAmount(e.target.value)
-    setAmountInFromCurrency(true)
+      setAmount(e.target.value)
+      setAmountInFromCurrency(true)
   }
 
   function handleToAmountChange(e) {
     setAmount(e.target.value)
     setAmountInFromCurrency(false)
-  }
+}
 
   return (
     <div className='flex justify-center items-center text-center'>
       <div>
-        <h1 className='text-3xl mb-2'>Currency Convertor</h1>
+        <h1 className='text-3xl mb-2'>Currency Converter</h1>
         <CurrencyRow  
-          currencyOptions={currencyOptions}
-          selectedCurrency={fromCurrency}
           amount={fromAmount}
-          onChangeCurrency={e => setFromCurrency(e.target.value)}
+          currencyOptions={currencyOptions}
+          defaultCurrency={fromCurrency}
+          onChangeCurrencySelector={e => setFromCurrency(e.target.value)}
           onChangeAmount={handleFromAmountChange}
         />
         <div className='text-6xl font-bold'>=</div>
         <CurrencyRow 
-          currencyOptions={currencyOptions}
-          selectedCurrency={toCurrency}
           amount={toAmount}
-          onChangeCurrency={e => setToCurrency(e.target.value)}
+          currencyOptions={currencyOptions}
+          defaultCurrency={toCurrency}
+          onChangeCurrencySelector={e => setToCurrency(e.target.value)}
           onChangeAmount={handleToAmountChange}
         />
       </div>
--- src/components/CurrencyRow.jsx
+++ src/components/CurrencyRow.jsx
@@ -1,23 +1,24 @@
 let CurrencyRow = (props) => {
     const {
         currencyOptions,
-        selectedCurrency,
-        onChangeCurrency,
-        onChangeAmount,
+        defaultCurrency,
+        onChangeCurrencySelector,
         amount,
+        onChangeAmount,
     } = props
     return (
         <div>
             <input 
+            value={amount}
             type="number" 
             className="border-solid border-gray-500 border-2 rounded m-2 p-1"
-            value={amount}
             onChange={onChangeAmount}
             />
             <select 
-            onChange={onChangeCurrency}
-            value={selectedCurrency}
-            className="ml-1">
+            className="ml-1"
+            value={defaultCurrency}
+            onChange={onChangeCurrencySelector}
+            >
                 {currencyOptions.map(option => (
                     <option key={option} value={option}>{option}</option>
                 ))}
